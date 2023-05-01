499e26045c4bc75fad9bf215151b466a96791829
Dahyun-K <da19950306@gmail.com>
2023-05-01 14:36:19 +0900

    feat: Create sample function

--- selector.py
+++ selector.py
@@ -1,12 +1,5 @@
 import random
 
-lotto = []
-rnd_num = random.randint(1, 45)
 
-for i in range(6):
-    while rnd_num in lotto:
-        rnd_num = random.randint(1,45)
-    lotto.append(rnd_num)
-
-lotto.sort()
-print(f'숫자 6개 선택하기 : {lotto}')
+result = random.sample(range(1,45+1), k=6)
+print(result)
